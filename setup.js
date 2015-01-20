79b98818476506377601c6e9b0893726a9465202
mike o'brien <mpobrien005@gmail.com>
2015-01-20 13:33:13 -0500

    fix typo in field name

--- setup.js
+++ setup.js
@@ -2,5 +2,5 @@ db.logs.createIndex({build_id:1, test_id:1, seq:1})
 db.logs.createIndex({started:1}, {expireAfterSeconds: 60*60*24*90}) // 90 days retention?
 db.tests.createIndex({started:1}, {expireAfterSeconds: 60*60*24*90}) // 90 days retention?
 db.builds.createIndex({started:1}, {expireAfterSeconds: 60*60*24*90}) // 90 days retention?
-db.builds.createIndex({build_num:1, builder:1})
+db.builds.createIndex({buildnum:1, builder:1})
 db.tests.createIndex({build_id:1, started:1})
