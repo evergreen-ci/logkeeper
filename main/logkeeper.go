f13a46a94fdcd5560723902afa00c1e5e8708cdd
Therese Avitabile <Tess@Thereses-MacBook-Pro.local>
2015-09-01 16:54:22 -0400

    EVG-513 add timestamps to logkeeper logs

--- main/logkeeper.go
+++ main/logkeeper.go
@@ -9,9 +9,30 @@ import (
 	"github.com/tylerb/graceful"
 	"gopkg.in/mgo.v2"
 	"time"
-
-	//"net/http"
+	"log"
+	"net/http"
+	"os"
 )
+
+type Logger struct {
+	*log.Logger
+}
+
+// Custom logger to label logs with [logkeeper] and timestamp
+func NewLogger() *Logger {
+	return &Logger{log.New(os.Stdout, "[logkeeper] ", 0)}
+}
+
+// Identical to negroni logger, but with timestamps added
+func (l *Logger) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	start := time.Now()
+	l.Printf(time.Now().Format(time.RFC3339) + " Started %s %s", r.Method, r.URL.Path)
+
+	next(rw, r)
+
+	res := rw.(negroni.ResponseWriter)
+	l.Printf(time.Now().Format(time.RFC3339) + " Completed %v %s in %v", res.Status(), http.StatusText(res.Status()), time.Since(start))
+}
 
 func main() {
 	var httpPort = flag.Int("port", 8080, "port to listen on for HTTP")
@@ -29,7 +50,10 @@ func main() {
 		URL: fmt.Sprintf("http://localhost:%v", *httpPort),
 	})
 	router := lk.NewRouter()
-	n := negroni.Classic()
+	n := negroni.New()
+	n.Use(NewLogger())
+	n.Use(negroni.NewRecovery()) // part of negroni Classic settings
+	n.Use(negroni.NewStatic(http.Dir("public"))) // part of negroni Classic settings
 	n.Use(gzip.Gzip(gzip.DefaultCompression))
 	n.UseHandler(router)
 
