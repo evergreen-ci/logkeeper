87e679fe63fcbc7954e718204577e75b901340a5
Therese Avitabile <Tess@Thereses-MacBook-Pro.local>
2015-09-02 10:37:11 -0400

    EVG-513 add timestamps to logkeeper app logs

--- main/logkeeper.go
+++ main/logkeeper.go
@@ -9,9 +9,43 @@ import (
 	"github.com/tylerb/graceful"
 	"gopkg.in/mgo.v2"
 	"time"
-
-	//"net/http"
+	"log"
+	"net/http"
+	"os"
 )
+
+// Logger is a middleware handler that logs the request as it goes in and the response as it goes out.
+type Logger struct {
+	// Logger inherits from log.Logger used to log messages with the Logger middleware
+	*log.Logger
+	// ids is a channel producing unique, autoincrementing request ids that are included in logs.
+	ids chan int
+}
+
+// NewLogger returns a new Logger instance
+func NewLogger() *Logger {
+	ids := make(chan int, 100)
+	go func() {
+		reqId := 0
+		for {
+			ids <- reqId
+			reqId++
+		}
+	}()
+
+	return &Logger{log.New(os.Stdout, "[logkeeper] ", log.Lmicroseconds), ids}
+}
+
+func (l *Logger) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	start := time.Now()
+	reqId := <-l.ids
+	l.Printf("Started (%v) %s %s %s", reqId, r.Method, r.URL.Path, r.RemoteAddr)
+
+	next(rw, r)
+
+	res := rw.(negroni.ResponseWriter)
+	l.Printf("Completed (%v) %v %s in %v", reqId, res.Status(), http.StatusText(res.Status()), time.Since(start))
+}
 
 func main() {
 	var httpPort = flag.Int("port", 8080, "port to listen on for HTTP")
@@ -29,7 +63,10 @@ func main() {
 		URL: fmt.Sprintf("http://localhost:%v", *httpPort),
 	})
 	router := lk.NewRouter()
-	n := negroni.Classic()
+	n := negroni.New()
+	n.Use(NewLogger())
+	n.Use(negroni.NewRecovery()) // part of negroni Classic settings
+	n.Use(negroni.NewStatic(http.Dir("public"))) // part of negroni Classic settings
 	n.Use(gzip.Gzip(gzip.DefaultCompression))
 	n.UseHandler(router)
 
