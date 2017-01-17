f9675dc00d7d4d58301f00e5993bd4683302ad3a
Sam Kleinman <sam@10gen.com>
2017-01-17 11:37:09 -0500

    EVG-1432: add vendored logging package

---
+++ vendor/github.com/tychoish/grip/send/stream.go
@@ -0,0 +1,49 @@
+package send
+
+import (
+	"strings"
+
+	"github.com/tychoish/grip/message"
+)
+
+// this file contains tools to support the slogger interface
+
+// WriteStringer captures the relevant part of the io.Writer interface
+// useful for writing log messages to streams.
+type WriteStringer interface {
+	WriteString(str string) (int, error)
+}
+
+type streamLogger struct {
+	fobj WriteStringer
+	*base
+}
+
+// NewStreamLogger produces a fully configured Sender that writes
+// un-formatted log messages to an io.Writer (or conforming subset).
+func NewStreamLogger(name string, ws WriteStringer, l LevelInfo) (Sender, error) {
+	return setup(MakeStreamLogger(ws), name, l)
+}
+
+// MakeStreamLogger constructs an unconfigured stream sender that
+// writes un-formatted log messages to the specified io.Writer, or
+// instance that implements a conforming subset.
+func MakeStreamLogger(ws WriteStringer) Sender {
+	return &streamLogger{
+		fobj: ws,
+		base: newBase(""),
+	}
+}
+
+func (s *streamLogger) Type() SenderType { return Stream }
+func (s *streamLogger) Send(m message.Composer) {
+	if s.level.ShouldLog(m) {
+		msg := m.String()
+
+		if !strings.HasSuffix(msg, "\n") {
+			msg += "\n"
+		}
+
+		_, _ = s.fobj.WriteString(msg)
+	}
+}
