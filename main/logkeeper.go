385f3d1de322cfec6e8d1f052687244bdc869d87
Sam Kleinman <sam@10gen.com>
2017-01-23 17:24:28 -0500

    EVG-1432: app log path should be specifiable

--- main/logkeeper.go
+++ main/logkeeper.go
@@ -44,6 +44,7 @@ func main() {
 	dbHost := flag.String("dbhost", "localhost:27017", "host/port to connect to DB server. Comma separated.")
 	rsName := flag.String("rsName", "", "name of replica set that the DB instances belong to. "+
 		"Leave empty for stand-alone and mongos instances.")
+	logPath := flag.String("logpath", "logkeeperapp.log", "path to log file")
 	maxRequestSize := flag.Int("maxRequestSize", 1024*1024*32,
 		"maximum size for a request in bytes, defaults to 32 MB (in bytes)")
 	flag.Parse()
@@ -53,7 +54,7 @@ func main() {
 		Threshold: level.Info,
 	}
 
-	sender, err := send.NewFileLogger("logkeeper", "logkeeperapp.log", sendLogLevels)
+	sender, err := send.NewFileLogger("logkeeper", *logPath, sendLogLevels)
 	grip.CatchEmergencyFatal(err)
 	defer sender.Close()
 
