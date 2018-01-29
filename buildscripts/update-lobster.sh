29143f9911e5111c86d52435e0324a2c321c645e
John Liu <john.liu@mongodb.com>
2018-01-29 13:45:34 -0500

    cr

--- buildscripts/update-lobster.sh
+++ buildscripts/update-lobster.sh
@@ -4,6 +4,7 @@
 # they can be served by logkeeper
 # it should only be run from the base logkeeper directory
 set -o errexit
+set -o xtrace
 
 SCRIPTS_DIR=buildscripts
 LOBSTER_REPO=https://github.com/evergreen-ci/lobster.git
@@ -16,29 +17,28 @@ LOGKEEPER_JS_DIR=js
 LOGKEEPER_CSS_DIR=css
 LOGKEEPER_TEMPLATES_DIR=templates/lobster/build
 
-# clone lobster repo and change to it
+# clone lobster repo
 pushd $SCRIPTS_DIR
 rm -rf $LOBSTER_DIR
 git clone $LOBSTER_REPO $LOBSTER_DIR
-pushd .lobster-temp
-
 # build lobster
+pushd $LOBSTER_DIR
 npm install
 npm run build
-
-# replace existing js/css files in logkeeper with the updated ones
 popd && popd
+
+# replace existing js/css/html files in logkeeper with the updated ones
 rm -rf $LOGKEEPER_STATIC_DIR/$LOGKEEPER_JS_DIR
 rm -rf $LOGKEEPER_STATIC_DIR/$LOGKEEPER_CSS_DIR
 cp -R $SCRIPTS_DIR/$LOBSTER_DIR/$LOBSTER_ASSETS_DIR/$LOBSTER_STATIC_DIR/ $LOGKEEPER_STATIC_DIR/
 cp $SCRIPTS_DIR/$LOBSTER_DIR/$LOBSTER_ASSETS_DIR/$LOBSTER_HTML $LOGKEEPER_TEMPLATES_DIR/
-pushd $LOGKEEPER_TEMPLATES_DIR
 
 # surround the html with go template tags
+pushd $LOGKEEPER_TEMPLATES_DIR
 echo -e "{{define \"base\"}}\n$(cat index.html)" > index.html
 echo "{{end}}" >> index.html
+popd
 
 # clean up temporary lobster directory
-popd
 rm -rf $SCRIPTS_DIR/$LOBSTER_DIR/
 echo "finished updating lobster"
