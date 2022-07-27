cfaf5e946ccf775c881e18b0ed1c24834ac8821d
John Daniels <john.daniels@mongodb.com>
2022-07-27 12:31:55 -0400

    EVG-17347: Update storage to use pail to create upload test data.
    We need to do this for the eventual use case of testing against
    S3.

--- storage/storage_test.go
+++ storage/storage_test.go
@@ -2,44 +2,24 @@ package storage
 
 import (
 	"context"
-	"io/fs"
-	"io/ioutil"
 	"os"
-	"path/filepath"
 	"testing"
-
-	"strings"
 
 	"github.com/evergreen-ci/pail"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func copyDirectory(source string, target string) error {
-	var err error = filepath.Walk(source, func(path string, info fs.FileInfo, err error) error {
-		var relPath string = strings.Replace(path, source, "", 1)
-		if relPath == "" {
-			return nil
-		}
-		if info.IsDir() {
-			return os.Mkdir(filepath.Join(target, relPath), 0755)
-		} else {
-			var data, fileReadErr = ioutil.ReadFile(filepath.Join(source, relPath))
-			if fileReadErr != nil {
-				return fileReadErr
-			}
-			return ioutil.WriteFile(filepath.Join(target, relPath), data, 0666)
-		}
-	})
-	return err
-}
-
 func makeTestStorage(t *testing.T, initDir string) Storage {
+	os.RemoveAll("../_bucketdata")
 	os.Mkdir("../_bucketdata", 0755)
-	copyDirectory(initDir, "../_bucketdata")
 	bucket, err := pail.NewLocalBucket(pail.LocalOptions{
 		Path:   "../_bucketdata",
 		Prefix: "",
+	})
+	bucket.Push(context.Background(), pail.SyncOptions{
+		Local:  initDir,
+		Remote: "/",
 	})
 	require.NoError(t, err)
 
