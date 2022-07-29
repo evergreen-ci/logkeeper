5dfc4aba5451a7218586d59359c6fd18558647e7
John Daniels <john.daniels@mongodb.com>
2022-07-29 09:31:12 -0400

    EVG-17156: Remove unused functions from time_range.go

--- models/time_range.go
+++ models/time_range.go
@@ -13,40 +13,10 @@ func (t TimeRange) Duration() time.Duration { return t.EndAt.Sub(t.StartAt) }
 func (t TimeRange) IsZero() bool            { return t.EndAt.IsZero() && t.StartAt.IsZero() }
 func (t TimeRange) IsValid() bool           { return t.Duration() >= 0 }
 
-// Check returns true if the given time is within the TimeRange (inclusive) and
-// false otherwise.
-func (t TimeRange) Check(ts time.Time) bool {
-	if (ts.After(t.StartAt) || ts.Equal(t.StartAt)) &&
-		(ts.Before(t.EndAt) || ts.Equal(t.EndAt)) {
-		return true
-	}
-	return false
-}
-
 // Check returns true if this TimeRange overlaps the other TimeRange
 func (t TimeRange) Intersects(other TimeRange) bool {
 	if t.EndAt.Before(other.StartAt) || t.StartAt.After(other.EndAt) {
 		return false
 	}
 	return true
-}
-
-// GetTimeRange builds a time range structure. If startAt is the zero
-// time, then end defaults to the current time and the start time is
-// determined by the duration. Otherwise the end time is determined
-// using the duration.
-func GetTimeRange(startAt time.Time, duration time.Duration) TimeRange {
-	var endTime time.Time
-
-	if startAt.IsZero() {
-		endTime = time.Now()
-		startAt = endTime.Add(-duration)
-	} else {
-		endTime = startAt.Add(duration)
-	}
-
-	return TimeRange{
-		StartAt: startAt,
-		EndAt:   endTime,
-	}
 }
